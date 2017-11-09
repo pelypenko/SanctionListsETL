87470440f92e0081fde66918213b7ed7d67537a6
Denys Pelypenko <pelypenkogmail.com>
2017-11-09 17:50:17 +0200

    AWS lambda ready

--- database/1.Create_WatchlistDB.sql
+++ database/1.Create_WatchlistDB.sql
@@ -1,11 +1,8 @@
-﻿-- Database: opendb
-
--- DROP DATABASE opendb;
-
-CREATE DATABASE watchlistdb
+﻿CREATE DATABASE watchlistdb
   WITH OWNER = postgres
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
        LC_COLLATE = 'English_United States.1252'
        LC_CTYPE = 'English_United States.1252'
        CONNECTION LIMIT = -1;
+
--- database/2.Create_Main_Objects.sql
+++ database/2.Create_Main_Objects.sql
@@ -1,24 +1,22 @@
-﻿
-CREATE SEQUENCE public.watchlisttypes_id_seq
+﻿CREATE SEQUENCE IF NOT EXISTS public.watchlisttypes_id_seq
   INCREMENT 1
   MINVALUE 1
   MAXVALUE 9223372036854775807
-  START 460
+  START 1
   CACHE 1;
 ALTER TABLE public.watchlisttypes_id_seq
   OWNER TO postgres;
 
-CREATE SEQUENCE public.watchlists_id_seq
+CREATE SEQUENCE IF NOT EXISTS public.watchlists_id_seq
   INCREMENT 1
   MINVALUE 1
   MAXVALUE 9223372036854775807
-  START 41004
+  START 1
   CACHE 1;
 ALTER TABLE public.watchlists_id_seq
   OWNER TO postgres;
 
-
-CREATE TABLE public.watchlisttypes
+CREATE TABLE IF NOT EXISTS public.watchlisttypes
 (
   id integer NOT NULL DEFAULT nextval('watchlisttypes_id_seq'::regclass),
   key text NOT NULL,
@@ -26,26 +24,14 @@ CREATE TABLE public.watchlisttypes
   active boolean NOT NULL DEFAULT true,
   CONSTRAINT "PK_watchlisttypes" PRIMARY KEY (id),
   CONSTRAINT "UQ_WatchListTypes_Key" UNIQUE (key),
-  CONSTRAINT ck_watchlisttypes_name_text CHECK (length(name::text) <= 256),
+  CONSTRAINT ck_watchlisttypes_name_text CHECK (length(name::text) <= 512),
   CONSTRAINT key CHECK (length(key::text) <= 64)
 )
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE public.watchlisttypes
-  OWNER TO postgres;
+WITH (OIDS=FALSE);
 
--- Index: public."IX_WatchListTypes_Active"
+ALTER TABLE public.watchlisttypes OWNER TO postgres;
 
--- DROP INDEX public."IX_WatchListTypes_Active";
-
-CREATE INDEX "IX_WatchListTypes_Active"
-  ON public.watchlisttypes
-  USING btree
-  (active);
-
-
-CREATE TABLE public.watchlists
+CREATE TABLE IF NOT EXISTS public.watchlists
 (
   id integer NOT NULL DEFAULT nextval('watchlists_id_seq'::regclass),
   watchlisttype_id integer NOT NULL,
@@ -60,36 +46,14 @@ CREATE TABLE public.watchlists
   createdon timestamp without time zone NOT NULL DEFAULT timezone('utc'::text, now()),
   modifiedon timestamp without time zone NOT NULL DEFAULT timezone('utc'::text, now()),
   indexeddata text,
+  namechanged boolean NOT NULL DEFAULT true,
   CONSTRAINT "PK_watchlists" PRIMARY KEY (id),
   CONSTRAINT "FK_WatchLists_WatchListTypeId" FOREIGN KEY (watchlisttype_id)
       REFERENCES public.watchlisttypes (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT ck_watchlists_dateofbirths_text CHECK (length(dateofbirths::text) <= 256),
   CONSTRAINT ck_watchlists_name_text CHECK (length(name::text) <= 1024),
   CONSTRAINT ck_watchlists_remoteid_text CHECK (length(remoteid::text) <= 64)
 )
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE public.watchlists
-  OWNER TO postgres;
+WITH (OIDS=FALSE);
 
--- Index: public."IX_WatchLists_IndexStatus"
-
--- DROP INDEX public."IX_WatchLists_IndexStatus";
-
-CREATE INDEX "IX_WatchLists_IndexStatus"
-  ON public.watchlists
-  USING btree
-  (indexstatus);
-
--- Index: public."IX_WatchLists_WatchListTypeAndRemoteId"
-
--- DROP INDEX public."IX_WatchLists_WatchListTypeAndRemoteId";
-
-CREATE INDEX "IX_WatchLists_WatchListTypeAndRemoteId"
-  ON public.watchlists
-  USING btree
-  (watchlisttype_id, remoteid COLLATE pg_catalog."default");
-
-
+ALTER TABLE public.watchlists OWNER TO postgres;
